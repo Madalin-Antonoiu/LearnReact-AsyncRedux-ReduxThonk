7cadadb31f057764a41fe45cf8777c448b27767c
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-05 18:33:42 +0000

    Second Approach : Action Creator within Action Creator. BETTER in my opinion.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,22 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-//function that returns a function
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
-
-  // Looking at the API, i only want the data obect property from response
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-// Perform only one fetch call per unique value ( limit the network calls from 100 to 10 in our case)
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
+
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+//Must call the other action creators within full redux cycle, within dispatch - dispatch(fetchPosts())
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //console.log("about to fetch posts");
+  await dispatch(fetchPosts()); // await here means "Don't continue with next line of code inside this function until this one is completed."
+  //console.log(getState().posts);
+  const userIds = _.uniq(_.map(getState().posts, "userId")); //getState() is from Redux
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};

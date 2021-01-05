afbff839636577577c63f9b984a9a9a3027b5418
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-05 15:55:56 +0000

    Memoize way finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 //function that returns a function
@@ -8,8 +9,10 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
 
+// Perform only one fetch call per unique value ( limit the network calls from 100 to 10 in our case)
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});

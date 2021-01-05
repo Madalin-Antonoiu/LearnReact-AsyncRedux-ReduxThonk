02b870f2fcd4615fc59d65b6f08c64dde38ed4f7
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-05 18:40:53 +0000

    Refactored fetchPostsAndUsers with _.chain() (optional)

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,18 +5,21 @@ export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
-
 export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
-
-//Must call the other action creators within full redux cycle, within dispatch - dispatch(fetchPosts())
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-  //console.log("about to fetch posts");
   await dispatch(fetchPosts()); // await here means "Don't continue with next line of code inside this function until this one is completed."
-  //console.log(getState().posts);
-  const userIds = _.uniq(_.map(getState().posts, "userId")); //getState() is from Redux
-  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  //Totally optional, same as below two lines
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId")); //getState() is from Redux
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
 };
